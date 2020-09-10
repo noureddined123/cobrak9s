e5130917025a8e601f5dde84f2cfc8a5b1774d9e
Noureddine <noureddined@users.noreply.github.com>
2020-09-10 12:38:10 +0200

    add app3 nodejs

--- images/app3njs/index.js
+++ images/app3njs/index.js
@@ -1,10 +1,26 @@
-const https = require('https');
-https.get('https://www.appdynamics.com/', (resp) => {
+//const https = require('https');
+const request = require('request');
 
-  resp.on('data', () => {
-    console.log('test');
-  });
-}).on("error", (err) => {
-  console.log("Error: " + err.message);
-});
+
+
+//function myFunc(){
+//    https.get('https://www.google.com/', (resp) => {
+//        resp.on('end', () => {
+//        console.log('test');
+//    });
+//    }).on("error", (err) => {
+//        console.log("Error: " + err.message);
+//    });
+//}
+
+
+    function test(){
+    request('https://jsonplaceholder.typicode.com/todos/1', { json: true }, (err, res, body) => {
+    if (err) { return console.log(err); }
+    console.log(body.title);
+    console.log(body.completed)
+
+    });
+    }
+    setInterval(test, 5000);
 
