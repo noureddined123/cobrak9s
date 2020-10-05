6da37b1fe45d0efc7f17995f6c93b18656f9305a
Noureddine <noureddined@users.noreply.github.com>
2020-10-05 13:43:17 +0200

    loadgen voor nodejs

--- images/loadgen/loadgen.py
+++ images/loadgen/loadgen.py
@@ -2,11 +2,13 @@ import requests, random, time
 
 
 url = "http://service1:5000"
+url2 = "http://service3:8080"
 
 
 while True:
     try:
         print requests.get(url)
+        print requests.get(url2)
 
     except Exception as e:
         print e
